cdc6a80bac071b7a008d93238c3be40ca9dca1d4
kevindoni <kevindoni17@gmail.com>
2021-06-13 15:38:24 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -15,10 +15,10 @@ ver=$VERSION_ID
 country=ID
 state=Indonesia
 locality=Indonesia
-organization=KEVIN DONI.xyz
-organizationalunit=KEVIN DONI.xyz
-commonname=KEVIN DONI.xyz
-email=admin@KEVIN DONI.xyz
+organization=EMBEX.ONLINE
+organizationalunit=EMBEX.ONLINE
+commonname=EMBEX.ONLINE
+email=ADMIN@EMBEX.ONLINE
 
 # simple password minimal
 wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/kevindoni/AutoScriptSSH/main/password"
